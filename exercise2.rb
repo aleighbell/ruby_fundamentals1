4805df0f917a3e3b458a24f7d67fd4822e68f9e8
aleighbell <aleighbell@gmail.com>
2016-08-28 07:43:35 -0400

    Add files via upload
    corrected entire assignment

--- exercise2.rb
+++ exercise2.rb
@@ -1,11 +1,14 @@
-puts "How to calculate a good tip for a 55 dollar meal"
-   puts "#{55+7.50}"
-     puts "A good tip is twenty percent"
-        puts "twenty + 45"
-           puts "twenty + forty five = #{65}"
-              puts "20+45"
-            puts "#{20+45}"
-          puts "twenty" + "45"
-        puts "answer: #{45628*7839} one bigass number"
-      puts (10<20 && 30<20) || !(10 == 11)
-    puts (red is black)
+#how do you calculate a good tip for a decent meal?
+#tips are usually 15%
+
+puts 50.00 * 0.15
+
+#how to add a string and intiger
+puts "Hello! I have" + 45_to.s + "leather bound books"
+
+#how to use string interpolation
+puts "how to calculate a large equation?"
+puts "answer: #{45628 * 7839} one bigass number"
+
+#the output of the following statement will return true!
+puts (10<20 && 30<20) || !(10 == 11)
